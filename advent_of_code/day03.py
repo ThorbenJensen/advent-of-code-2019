9ca9b6cf195853eee9d76fde909d3de69f0d8d92
Thorben Jensen <jensen.thorben@gmail.com>
2020-05-17 19:58:54 +0200

    clean up draft

--- advent_of_code/day03.py
+++ advent_of_code/day03.py
@@ -1,10 +1,9 @@
-from copy import copy
 from typing import List, Tuple
 
 import numpy as np
 from functional import seq
 
-CENTRAL_PORT = (10, 10)  # (row, column)
+CENTRAL_PORT = (1000, 1000)  # (row, column)
 
 
 def input_to_directions(input: str) -> List[Tuple[str, int]]:
@@ -13,7 +12,7 @@ def input_to_directions(input: str) -> List[Tuple[str, int]]:
     return directions
 
 
-def vector_for_direction(direction: Tuple[str, int]) -> Tuple[int]:
+def direction_to_vector(direction: Tuple[str, int]) -> Tuple[int]:
     direction_string = direction[0]
     if direction_string == "U":
         return (-1, 0)
@@ -26,39 +25,47 @@ def vector_for_direction(direction: Tuple[str, int]) -> Tuple[int]:
     raise ValueError
 
 
-def draw_wire(field: np.ndarray, input: str) -> np.ndarray:
+def get_empty_grid() -> np.ndarray:
+    grid = np.zeros(shape=tuple(e * 2 for e in CENTRAL_PORT))
+    grid[CENTRAL_PORT] = 1
+    return grid
+
+
+def draw_wire(input: str) -> np.ndarray:
+    grid = get_empty_grid()
     directions: List[Tuple[str, int]] = input_to_directions(input)
-    field_wire = copy(field)
     position = CENTRAL_PORT
     for direction in directions:
-        vector = vector_for_direction(direction)
+        vector = direction_to_vector(direction)
         # print(vector)
         for i in range(direction[1]):
             position = [position[i] + vector[i] for i in range(len(position))]
             # print(position)
-            field_wire[(position[0], position[1])] = 1
-    return field_wire
+            grid[(position[0], position[1])] = 1
+    return grid
 
 
-a = np.zeros(shape=tuple(e * 2 for e in CENTRAL_PORT))
-a[CENTRAL_PORT] = "1"
+def manhattan_distance(port: Tuple[int, int]):
+    return abs(CENTRAL_PORT[0] - port[0]) + abs(CENTRAL_PORT[1] - port[1])
+
+
+def combined_wiring(inputs: List[str]):
+    wiring1 = draw_wire(input=inputs[0])
+    wiring2 = draw_wire(input=inputs[1])
+    wiring_combined = wiring1 + wiring2
+    return wiring_combined
+
+
+def distance_closest_crossing():
+    crossings = seq((zip(*np.where(grid_combined > 1)))).filter(
+        lambda x: x != CENTRAL_PORT
+    )
+    closest_distance = crossings.map(manhattan_distance).min()
+    return closest_distance
 
 
 input1 = "R8,U5,L5,D3"
 input2 = "U7,R6,D4,L4"
-
-wiring1 = draw_wire(field=a, input=input1)
-wiring2 = draw_wire(field=a, input=input2)
-
-wiring = wiring1 + wiring2
-print(wiring)
-
-crossings = seq((zip(*np.where(wiring > 1)))).filter(lambda x: x != CENTRAL_PORT)
-
-
-def manhattan_distance(port: Tuple[int, int]):
-    return (abs(CENTRAL_PORT[0] - port[0]) + abs(CENTRAL_PORT[1] - port[1]))
-
-
-closest_distance = crossings.map(manhattan_distance).min()
+grid_combined = combined_wiring(inputs=[input1, input2])
+closest_distance = distance_closest_crossing()
 print(f"closest_distance: {closest_distance}")
